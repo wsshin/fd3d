40b9a4f3603a12fc73c0c5ba949075a675dc46eb
Wonseok Shin <wsshin97@gmail.com>
2013-09-16 21:57:24 -0700

    FD3D is modified to support 4 equations

--- src/include/type.h
+++ src/include/type.h
@@ -4,10 +4,11 @@
 typedef enum {VBCompact, VBMedium, VBDetail} VerboseLevel;
 typedef enum {Xx, Yy, Zz, Naxis} Axis;
 typedef enum {Neg, Pos, Nsign} Sign;
-typedef enum {Etype, Htype} FieldType;
+typedef enum {Prim, Dual, Ngt} GridType;
+typedef enum {Etype, Htype, Nft} FieldType;
 typedef enum {PEC, PMC, Bloch} BC;
 typedef enum {SCPML, UPML} PMLType;
-typedef enum {PCIdentity, PCSparam, PCEps, PCJacobi} PrecondType;
+typedef enum {PCIdentity, PCSfactor, PCParam, PCJacobi} PrecondType;
 typedef enum {BiCG, QMR} KrylovType;
 typedef enum {Rr, Ii, Nri} RI;
 
